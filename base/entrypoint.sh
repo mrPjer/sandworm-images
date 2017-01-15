10b5f65ca801f375d0f0492fbaa7a5385a8e2740
Petar Šegina <psegina@ymail.com>
2017-01-15 18:32:11 +0100

    Log all output and error with the stream collector

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -1,6 +1,13 @@
+LOGGER_ENDPOINT="$1"
+
 if [ -f ./stdin ]
 	then
-		./runner.sh < ./stdin
+		./runner.sh \
+		< ./stdin \
+		1> >(tee >(./stream-collector.sh "$LOGGER_ENDPOINT" stdout)) \
+		2> >(tee >(./stream-collector.sh "$LOGGER_ENDPOINT" stderr) >&2)
 	else
-		./runner.sh
+		./runner.sh \
+		1> >(tee >(./stream-collector.sh "$LOGGER_ENDPOINT" stdout)) \
+		2> >(tee >(./stream-collector.sh "$LOGGER_ENDPOINT" stderr) >&2)
 fi
