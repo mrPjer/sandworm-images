367b56c18a2ed106d2077094e0ea9c7ab6ee7e91
Petar Šegina <psegina@ymail.com>
2017-01-15 21:45:01 +0100

    Properly collect log
    The old implementation would kill the logging subproceses before they
    had a chance to finish output.
    
    https://stackoverflow.com/a/31151808/576809

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -1,13 +1,7 @@
 LOGGER_ENDPOINT="$1"
 
-if [ -f ./stdin ]
-	then
-		./runner.sh \
-		< ./stdin \
-		1> >(tee >(./stream-collector.sh "$LOGGER_ENDPOINT" stdout)) \
-		2> >(tee >(./stream-collector.sh "$LOGGER_ENDPOINT" stderr) >&2)
-	else
-		./runner.sh \
-		1> >(tee >(./stream-collector.sh "$LOGGER_ENDPOINT" stdout)) \
-		2> >(tee >(./stream-collector.sh "$LOGGER_ENDPOINT" stderr) >&2)
-fi
+set -e;
+
+touch ./stdin;
+
+{ ./runner.sh 2>&1 1>&3 3>&- < ./stdin | ./stream-collector.sh "$LOGGER_ENDPOINT" stderr; } 3>&1 1>&2 | ./stream-collector.sh "$LOGGER_ENDPOINT" stdout;
--- base/stream-collector.sh
+++ base/stream-collector.sh
@@ -3,11 +3,16 @@ LOGGER_TAG="$2"
 
 while IFS= read -r line || [[ -n "$line" ]];
 do
+	if [ "$LOGGER_TAG" == "stderr" ]; then
+		echo $line >&2;
+	else
+		echo $line;
+	fi
+
 	curl \
 		"$LOGGER_ENDPOINT" \
-		--silent \
+		-vvv \
 		--data-urlencode "tag=$LOGGER_TAG" \
 		--data-urlencode "content=$line" \
-		--data-urlencode "timestamp=`date --iso-8601=s`" \
-	> /dev/null
+		--data-urlencode "timestamp=`date --iso-8601=s`"
 done
